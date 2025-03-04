ae8d5bc4ebf41baa4346c3297db4e904c0bba848
Zurgambayev <polveko50@mail.ru>
2025-03-04 22:14:54 +0500

    erw

--- Tinder/routes/api.php
+++ Tinder/routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function(){
     Route::post('/profile', [ProfileController::class, 'store']);    // Создать профиль
     Route::get('/profile/{id}', [ProfileController::class, 'show']); // Получить профиль
     Route::put('/profile/{id}', [ProfileController::class, 'update']); // Обновить профиль
-    Route::delete('/profile/{id}', [ProfileController::class, 'destroy']); // Удалить профиль
+    Route::delete('/profile/{id}', [ProfileController::class, 'destroy']);
 });
